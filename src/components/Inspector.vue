c43212be81adfa300f3624d99fdb7517aee55bb1
Simon Porritt <simon@jsplumbtoolkit.com>
2025-04-27 09:20:20 +1000

    initial import

---
+++ src/components/Inspector.vue
@@ -0,0 +1,84 @@
+<template>
+    <div ref="container">
+        <div class="jtk-orgchart-inspector" v-if="current != null">
+            <h1>{{current.data.name}}</h1>
+            <h2>{{current.data.title}}</h2>
+            <h5 v-if="reports.length > 0">Reports:</h5>
+            <a v-for="r in reports" :key="r.id" class="jtk-orgchart-inspector-person" href="#" :data-id="r.data.id" @click="selectPerson(r)">
+                <img :src="getImage(r)" :alt="r.data.name"/>
+                <div>
+                    {{r.data.name}}
+                    <span>{{r.data.title}}</span>
+                </div>
+            </a>
+            <h5 v-if="manager != null">Reports to:</h5>
+            <a v-if="manager != null" class="jtk-orgchart-inspector-person" href="#" :data-id="manager.data.id" @click="selectPerson(manager)">
+                <img :src="getImage(manager)" :alt="manager.data.name"/>
+                <div>
+                    {{manager.data.name}}
+                    <span>{{manager.data.title}}</span>
+                </div>
+            </a>
+        </div>
+    </div>
+</template>
+<script>
+
+    import { nextTick } from "vue"
+    import {loadSurface, DEFAULT_VUE_SURFACE_ID} from "@jsplumbtoolkit/browser-ui-vue3";
+    import { Inspector } from "@jsplumbtoolkit/browser-ui"
+
+    function retrieveDirectReports(person){
+        return person.getSourceEdges().map(e => e.target)
+    }
+
+    function getManager(person) {
+        return person.getTargetEdges().map(e => e.source)[0]
+    }
+
+    export default {
+        data:() => {
+            return {
+                current:null,
+                reports:[],
+                manager:null,
+                inspector:null
+
+            }
+        },
+        methods:{
+          getImage:function(p) {
+              return `/avatars/${p.data.img}`
+          }
+        },
+        props:{
+            surfaceId:String,
+            selectPerson:Function
+        },
+        mounted() {
+            loadSurface(DEFAULT_VUE_SURFACE_ID, (surface) => {
+
+                // create an inspector. give it the container element
+                // and the surface and functions to use to render an
+                // empty container (no selection) and to render an editor.
+                // here we toggle `currentType` and let the template draw what
+                // it needs to.
+                this.inspector = new Inspector({
+                    container:this.$refs.container,
+                    surface,
+                    renderEmptyContainer:() => {
+                        this.reports.length = 0
+                        this.manager = null
+                    },
+                    refresh:(obj, cb) => {
+                        this.current = obj
+                        this.reports = retrieveDirectReports(obj)
+                        this.manager = getManager(obj)
+                        nextTick()
+                    }
+                })
+
+            })
+        }
+    }
+</script>
---
+++ src/components/OrgChart.vue
@@ -0,0 +1,100 @@
+<template>
+    <div style="width:100%;height:100%;display:flex">
+        <div class="jtk-demo-canvas">
+            <SurfaceComponent ref="toolkitComponent"
+                             :renderOptions="this.renderParams()"
+                             :viewOptions="this.viewParams()"
+                             url="dataset.json">
+
+            </SurfaceComponent>
+            <MiniviewComponent/>
+        </div>
+        <div class="jtk-demo-rhs">
+            <div id="inspector">
+                <Inspector v-bind:selectPerson="selectPerson"></Inspector>
+            </div>
+            <div class="description">
+
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+    import {defineComponent } from "vue"
+
+    import { HierarchyLayout, BlankEndpoint, EVENT_CANVAS_CLICK, DEFAULT, PlainArrowOverlay, AnchorLocations, EVENT_TAP } from "@jsplumbtoolkit/browser-ui"
+
+    import PersonComponent from "./PersonComponent.vue";
+    import Inspector from './Inspector.vue'
+
+    let toolkitComponent
+    let toolkit
+    let surface
+
+    export default defineComponent({
+        name:"orgchart",
+        components: {Inspector},
+        mounted() {
+
+            toolkitComponent = this.$refs.toolkitComponent;
+            toolkit = toolkitComponent.toolkit;
+            surface = toolkitComponent.surface;
+        },
+        methods:{
+            selectPerson:function(p) {
+                toolkit.setSelection(p)
+                surface.centerOnAndZoom(p, 0.15)
+            },
+            viewParams:function() {
+                return {
+                    nodes:{
+                        [DEFAULT]:{
+                            events:{
+                                [EVENT_TAP]:(p) => {
+                                    this.selectPerson(p.obj)
+                                }
+                            },
+                            component:PersonComponent
+                        }
+
+                    },
+                    edges:{
+                        default:{
+                            overlays:[
+                                {
+                                    type:PlainArrowOverlay.type,
+                                    options:{
+                                        location:1,
+                                        width:10,
+                                        length:10
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            },
+            renderParams:function() {
+                return {
+                    consumeRightClick:false,
+                    elementsDraggable:false,
+                    defaults:{
+                        endpoint:BlankEndpoint.type,
+                        anchor:AnchorLocations.ContinuousTopBottom
+                    },
+                    events: {
+                        [EVENT_CANVAS_CLICK]: function(e) {
+                            toolkit.clearSelection()
+                        }
+                    },
+                    zoomToFit:true,
+                    layout:{
+                        type: HierarchyLayout.type
+                    }
+                }
+            }
+        }
+    })
+
+</script>
